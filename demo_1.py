2a4e3fd236ee9896b588a420077cd7054510646a
antiwari <antiwari@ciena.com>
2019-12-16 22:30:25 +0530

    added new files

--- demo_1.py
+++ demo_1.py
@@ -1,1 +1,10 @@
 ### demo file 1
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt # Load an color image in grayscale
+img = cv2.imread('test_image/DSC_0345.JPG',0)
+
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
---
+++ demo_2.py
@@ -0,0 +1,19 @@
+### Capture Video from Camera
+
+import numpy as np
+import cv2
+
+cap=cv2.VideoCapture(-1)
+
+while(True):
+    ## capture frame-by-frame
+    ret,frame=cap.read()
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BAYER_BG2BGR)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
